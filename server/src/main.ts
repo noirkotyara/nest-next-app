da7f6c4f6ab093937ebaec72cc493b06fc5c734e
maliukh <noirkotyara@gmail.com>
2022-03-14 12:57:08 +0200

    add env variables for hosts

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ credentials: true, origin: 'http://localhost:3005' });
+  app.enableCors({ credentials: true, origin: ['http://localhost:3005', process.env.CLIENT_SERVER_HOST] });
 
   await app.listen(8081);
 }
