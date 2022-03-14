5a1a7ceff7534ed7f925d008893c7c10e5b3bcc6
maliukh <noirkotyara@gmail.com>
2022-03-14 21:05:17 +0200

    allow cors only for specific

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,10 +8,24 @@ require('dotenv').config({
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const whitelist = [
+    'https://nest-next-app.vercel.app/',
+    'https://localhost:3005',
+  ];
   app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
     credentials: true,
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
   await app.listen(8081);
