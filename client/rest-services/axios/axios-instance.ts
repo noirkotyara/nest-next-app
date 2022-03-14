f739ec038d2198d9a65f73dedb86a5775fa4c185
maliukh <noirkotyara@gmail.com>
2022-03-14 12:44:15 +0200

    add sell server host for client service

--- client/rest-services/axios/axios-instance.ts
+++ client/rest-services/axios/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: process.env.SELL_SERVER_HOST ?? "http://localhost:8081",
   withCredentials: true,
 });
