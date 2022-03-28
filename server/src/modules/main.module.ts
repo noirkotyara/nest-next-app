9b873bf24e09fb18319bb36716089612bdee9e2a
maliukh <noirkotyara@gmail.com>
2022-03-28 18:36:06 +0300

    add ProductModule

--- server/src/modules/main.module.ts
+++ server/src/modules/main.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DataServicesModule } from '../services/data-services/data-services/data-services.module';
-import { ProductModule } from './product.module';
 import { AppModule } from './app.module';
-import { AppController } from '../../controllers/app/app.controller';
-import { AppServicesService } from '../services/use-cases/app/app-services.service';
 import { appConfig } from '../../configs';
+import { ProductModule } from './product.module';
 
 @Module({
   imports: [
@@ -14,8 +12,8 @@ import { appConfig } from '../../configs';
       isGlobal: true,
       load: [appConfig],
     }),
-    // DataServicesModule,
-    // ProductModule,
+    DataServicesModule,
+    ProductModule,
     AppModule,
   ],
   controllers: [],
