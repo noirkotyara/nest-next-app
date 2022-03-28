ea0de90eb6394aefb17f8efb4026f46fb57ebfc1
maliukh <noirkotyara@gmail.com>
2022-03-28 12:53:24 +0300

    add index in configs but remove alias

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { appConfig } from 'configs/app.config';
+import { appConfig } from '../../../configs';
 
 @Module({
   imports: [
