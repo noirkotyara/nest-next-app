b159c1051fc746dd5238e50f910f34817bcd2822
maliukh <noirkotyara@gmail.com>
2022-03-28 11:23:11 +0300

    remove ProductHttpModule

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { appConfig, ormConfig } from 'configs';
+import { appConfig } from 'configs';
 import { ProductHttpModule } from '../product/http/product-http.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-// require('dotenv').config({
-//   path: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
-// });
 
 @Module({
   imports: [
@@ -27,7 +23,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    ProductHttpModule,
+    // ProductHttpModule,
   ],
   controllers: [AppController],
   providers: [AppService],
